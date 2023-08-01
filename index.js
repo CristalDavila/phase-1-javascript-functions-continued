2efcd164b757c866240243ca382a4d005f284190
Cristal Davila <cristal.davila@gmail.com>
2023-08-01 16:28:24 -0600

    functions part deux

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+function saturdayFun(activity="roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(work1= "go to the office", work2= "work from home"){
+    return `This Monday, I will ${work1}.`
+}
+function wrapAdjective(string="*") {
+    return function(parameter="special") {
+        return `You are ${string}${parameter}${string}!`}
+    }
